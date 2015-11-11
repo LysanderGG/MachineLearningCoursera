ca6757e211e56413a72c8e497f6ce66459afa619
Lysander <lysandergc@gmail.com>
2015-11-11 22:03:51 +0900

    Linear regression - second part - linear regression with multiple variables

--- LinearRegression/gradientDescentMulti.m
+++ LinearRegression/gradientDescentMulti.m
@@ -17,15 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+    delta = 1/m * sum((X * theta - y)' * X, m)';
+    theta = theta - alpha * delta;
 
     % ============================================================
 
